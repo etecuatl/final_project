b450fea905e19cb8df7627f7d71de37978aa6b80
etecuatl <tecuatl.erick@gmail.com>
2024-12-11 00:30:01 -0500

    updating class and begin formatting data

--- API_connection.py
+++ API_connection.py
@@ -2,51 +2,47 @@ import erick_API_key
 import requests
 import pandas as pd
 
-class API_connection:
+class API_Connection:
     '''This is setting up the connection to Polygon.io'''
     def __init__(self):
         #api key is stored in the file erick_API_key with contents being key = ''
         self.key = erick_API_key.key
+
+    def _hide_key(self,url):
+        '''Hides key from URL'''
+        return url.replace(self.key,"******")
     
-    def ticker1_connection(self,ticker1,start_date1,end_date1):
-        url1 = f'https://api.polygon.io/v2/aggs/ticker/{ticker1}/range/1/day/{start_date1}/{end_date1}?adjusted=true&sort=asc&apiKey={self.key}'
-        print(f'Fetching response from url.')
-        response = requests.get(url1)
+    def api_key(self):
+        '''Accessing key'''
+        return self.key
+
+class Ticker_Connection(API_Connection):
+    '''Uses Key to build url and call API'''
+    def ticker_connection(self,ticker,start_date,end_date):
+        url = self._url_parts(ticker,start_date,end_date)
+        hidden_url = self._hide_key(url)
+        print(f"Hidden Url: {hidden_url}")
+        response = requests.get(url)
         data = response.json()
-        print(f'Response: {data}')
+        df = pd.DataFrame()
         if response.status_code == 200:
-            data_ticker1 = response.json()
-            #error handling
-            if isinstance(data_ticker1,dict) and 'results' in data:
+            print(f"Received API Response.  Code {response.status_code}")
+            data_ticker = response.json()
+            if isinstance(data_ticker,dict) and 'results' in data:
                 results = data['results']
                 if results:
-                    df1 = pd.DataFrame(results)
-                    print("Data Found")
+                    #inserts and cleans data.
+                    df = pd.DataFrame(results)
+                    df.rename(columns={'t': 'Date', 'o': 'Open', 'h': 'High', 'l': 'Low', 'c': 'Close', 'v': 'Volume'}, inplace=True)
+                    #print(df.head())
+                else:
+                    print("No Results Found.")
             else:
-                print("Invalid Ticker.")
-                df1 = pd.DataFrame()
+                print("Wrong Format.")
         else:
-            print('Failed to fetch data from the API, or invalid Ticker.  Try again.')
-            df1 = pd.DataFrame()
-        return df1
+            print(f"Invalid Response. Code: {response.status_code}")
+        return df
     
-    def ticker2_connection(self,ticker2,start_date2,end_date2):
-        url2 = f'https://api.polygon.io/v2/aggs/ticker/{ticker2}/range/1/day/{start_date2}/{end_date2}?adjusted=true&sort=asc&apiKey={self.key}'
-        print(f'Fetching response from url.')
-        response = requests.get(url2)
-        data2 = response.json()
-        print(f'Response: {data2}')
-        if response.status_code == 200:
-            data_ticker2 = response.json()
-            #error handling
-            if isinstance(data_ticker2,dict) and 'results' in data2:
-                results = data2['results']
-                if results:
-                    df2 = pd.DataFrame(results)
-            else:
-                print("Invalid Ticker.")
-                df2 = pd.DataFrame()
-        else:
-            print('Failed to fetch data from the API, or invalid Ticker.  Try again.')
-            df2 = pd.DataFrame()
-        return df2
+    def _url_parts(self,ticker,start_date,end_date):
+        return(f'https://api.polygon.io/v2/aggs/ticker/{ticker.upper()}/range/1/day/{start_date}/{end_date}?adjusted=true&sort=asc&apiKey={self.api_key()}')
+    
